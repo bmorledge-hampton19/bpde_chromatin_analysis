9cbf74495a326a5c3a580ba69d0d5bb364a3ab67
Benjamin Morledge-Hampton <brotherbenben@yahoo.com>
2024-03-05 19:29:00 -0800

    Revamped plotting around a generalized function
    - Created the generalPlotting function for plotting an arbitrary number of features together.
    - Created the Plottable class for keeping track of information for each plottable data set.
    - Removed previous plotting functions.
    - Added plotting functions for plotting damage and repair together, plotting a single feature, and plotting features with custom nucleosome regions, all using the generalPlotting function.
    - Also created functions for plotting read length distributions, enriched indices, and individual nucleotide frequencies. (All related to tXR-seq lesion calling)

--- bpde_chromatin_analysis/PlottingFunctions.py
+++ bpde_chromatin_analysis/PlottingFunctions.py
@@ -1,64 +1,214 @@
 # A suite of plotting functions for the BPDE analysis.
 import pandas
+from enum import Enum
+from typing import List, Dict
 from plotnine import *
-from benbiohelpers.Plotting.PlotnineHelpers import defaultTextScaling, blankBackground
+from benbiohelpers.Plotting.PlotnineHelpers import defaultTextScaling, blankBackground, flushXAxis
+from mutperiodpy.GeneratePlotnineFigures import NucleosomeColorPalette
 
 
-def plotTFBSData(damageData: pandas.DataFrame, repairData: pandas.DataFrame, dataColumn = "Normalized_Both_Strands",
-                 title = "TFBS Damage vs. Repair", xAxisLabel = "Position Relative to Midpoint (bp)", yAxisLabel = "Normalized Damage/Repair Activity",
-                 xlim = None, ylim = None, dropZeroRows = True, damageColor = "#707070", repairColor = "#F1A226"):
+class Plottable:
     """
-    Plots repair and damage data relative to transcription factor binding site midpoints.
+    Wraps together the necessary information for plotting a data set, so it easier to pass to the generalPlotting function.
     """
 
-    assert damageData is not None or repairData is not None
+    def __init__(self, dataFrame: pandas.DataFrame, dataColumn: str, label,
+                 color, underlaidColor = None, colorColumn = None, underlaidColorColumn = None,
+                 colorColumnBreaksAndLabels: Dict[str,str] = None,
+                 smoothData = False, overlaySmoothedAndNormal = False):
+        self.dataFrame = dataFrame.copy()
+        self.dataColumn = dataColumn
+        self.smoothedDataColumn = dataColumn + "_Smoothed"
+        self.label = label
+        self.color = color
+        self.underlaidColor = underlaidColor
+        self.colorColumn = colorColumn
+        self.underlaidColorColumn = underlaidColorColumn
+        self.columnColorBreaksAndLabels = colorColumnBreaksAndLabels
+        self.smoothData = smoothData
+        self.overlaySmoothedAndNormal = overlaySmoothedAndNormal
+        
+
+def generalPlotting(plottables: List[Plottable],
+                     title = " ", xAxisLabel = "Position Relative to Feature (bp)", yAxisLabel = "Normalized Activity",
+                     xlim = None, ylim = None, dropZeroRows = True, smoothData = True, overlaySmoothedAndNormal = True):
+    """
+    Generalized plotting function for plotting an arbitrary number of features together (e.g., damage and repair).
+    """
 
     # Get rid of any rows with no counts.
     if dropZeroRows:
-        if damageData is not None:
-            damageData = damageData.loc[damageData[dataColumn] != 0].copy()
-        if repairData is not None:
-            repairData = repairData.loc[repairData[dataColumn] != 0].copy()
+        for plottable in plottables:
+            plottable = plottable.dataFrame.loc[plottable.dataFrame[plottable.dataColumn] != 0].copy()
+
+    # If requested, smooth the data.
+    if smoothData:
+        for plottable in plottables:
+            plottable.dataFrame[plottable.smoothedDataColumn] = plottable.dataFrame[plottable.dataColumn].rolling(11, center = True, min_periods=1).mean()
 
     # Generate the plot.
-    TFBSPlot = ggplot()
+    plot = ggplot(mapping = aes(x = "Dyad_Position"))
 
-    if damageData is not None:
-        TFBSPlot = TFBSPlot + geom_path(aes(x = "Dyad_Position", y = dataColumn, group = 1, color = 'damageColor'),
-                                        data = damageData, size = 1, lineend = "round")
-    if repairData is not None:
-        TFBSPlot = TFBSPlot + geom_path(aes(x = "Dyad_Position", y = dataColumn, group = 1, color = 'repairColor'),
-                                        data = repairData, size = 1, lineend = "round")
+    if overlaySmoothedAndNormal:
+        for plottable in plottables:
+            if plottable.underlaidColorColumn is not None: underlaidColorString = plottable.underlaidColorColumn
+            else: underlaidColorString = f"'{plottable.underlaidColor}'"
 
-    if repairData is not None and damageData is not None:
-        TFBSPlot = (TFBSPlot +
-            scale_color_identity(guide = "legend", name = " ", breaks = (damageColor,repairColor), labels = ("damage","repair")) +
+            plot = plot + geom_path(aes(y = plottable.dataColumn, color = f"{underlaidColorString}"), plottable.dataFrame, size = 1, group = 1, lineend = "round")
+
+    for plottable in plottables:
+        if plottable.colorColumn is not None: colorString = plottable.colorColumn
+        else: colorString = f"'{plottable.color}'"
+
+        if smoothData:
+            plot = plot + geom_path(aes(y = plottable.smoothedDataColumn, color = f"{colorString}"), plottable.dataFrame, size = 1, group = 1, lineend = "round")
+        else:
+            plot = plot + geom_path(aes(y = plottable.dataColumn, color = f"{colorString}"), plottable.dataFrame, size = 1, group = 1, lineend = "round")
+
+    if len(plottables) > 1:
+        plot = (plot +
+            scale_color_identity(guide = "legend", name = " ",
+                                 breaks = [plottable.color for plottable in plottables],
+                                 labels = [plottable.label for plottable in plottables]) +
             theme(figure_size = (12,6))
         )
-    else: TFBSPlot = TFBSPlot + theme(figure_size=(9,6))
+    elif plottables[0].columnColorBreaksAndLabels is not None:
+        plot = (plot +
+            scale_color_identity(guide = "legend", name = " ",
+                                 breaks = list(plottables[0].columnColorBreaksAndLabels.keys()),
+                                 labels = list(plottables[0].columnColorBreaksAndLabels.values())) +
+            theme(figure_size = (12,6))
+        )
+    else: plot = plot + scale_color_identity(guide = None, name = " ") + theme(figure_size=(9,6))
 
-    TFBSPlot = (TFBSPlot + 
+    plot = (plot + 
         coord_cartesian(xlim = xlim, ylim = ylim) +
         xlab(xAxisLabel) + ylab(yAxisLabel) + ggtitle(title) +
         defaultTextScaling + blankBackground
     )
 
-    return TFBSPlot
+    return plot
 
 
-def plotTSSData(damageData: pandas.DataFrame, repairData: pandas.DataFrame, dataColumn = "Normalized_Both_Strands",
-                title = "TSS Damage vs. Repair", xAxisLabel = "Position Relative to TSS (bp)", yAxisLabel = "Normalized Damage/Repair Activity",
-                xlim = None, ylim = None, damageColor = "#707070", repairColor = "#F1A226"):
-    """
-    Turns out, this works pretty much the same as the TFBS data!
-    """
-    return plotTFBSData(damageData, repairData, dataColumn, title, xAxisLabel, yAxisLabel, xlim, ylim, False, damageColor, repairColor)
+class PlottingColors(Enum):
+
+    BLACK = "#000000"
+    BLACK_UNDERLAY = "#BFBFBF"
+    DAMAGE = "#707070"
+    DAMAGE_UNDERLAY = "#CCCCCC"
+    REPAIR = "#F29500"
+    REPAIR_UNDERLAY = "#F2DBB6"
+    MUTATION = "#CC0000"
+    MUTATION_UNDERLAY = "#CC9999"
 
 
-def plotMiscData(data: pandas.DataFrame, dataColumn = "Normalized_Both_Strands",
-                 title = "Misc Data", xAxisLabel = "Position Relative to Feature (bp)", yAxisLabel = "Counts",
-                 xlim = None, ylim = None, color = "black"):
+def plotDamageAndRepair(damageData: pandas.DataFrame, repairData: pandas.DataFrame, dataColumn = "Normalized_Both_Strands",
+                        title = "Damage vs. Repair", xAxisLabel = "Position Relative to Feature (bp)", yAxisLabel = "Normalized Damage/Repair Activity",
+                        xlim = None, ylim = None, dropZeroRows = True, smoothData = True, overlaySmoothedAndNormal = True,
+                        damageColor = PlottingColors.DAMAGE.value, repairColor = PlottingColors.REPAIR.value,
+                        underlaidDamageColor = PlottingColors.DAMAGE_UNDERLAY.value, underlaidRepairColor = PlottingColors.REPAIR_UNDERLAY.value):
     """
-    Yup. This one just uses the TFBS plotting function again.
+    Plot damage and repair together using the generalizable plotting function
     """
-    return plotTFBSData(data, None, dataColumn, title, xAxisLabel, yAxisLabel, xlim, ylim, False, color)
+    damagePlottable = Plottable(damageData, dataColumn, "Damage", damageColor, underlaidDamageColor)
+    repairPlottable = Plottable(repairData, dataColumn, "Repair", repairColor, underlaidRepairColor)
+    return generalPlotting([damagePlottable, repairPlottable],
+                           title, xAxisLabel, yAxisLabel, xlim, ylim,
+                           dropZeroRows, smoothData, overlaySmoothedAndNormal)
+
+
+def plotSingleFeature(data: pandas.DataFrame, dataColumn = "Normalized_Both_Strands",
+                      title = "Misc Data", xAxisLabel = "Position Relative to Feature (bp)", yAxisLabel = "Counts",
+                      xlim = None, ylim = None, dropZeroRows = True, smoothData = True, overlaySmoothedAndNormal = True,
+                      color = PlottingColors.BLACK.value, underlaidColor = PlottingColors.BLACK_UNDERLAY.value):
+    """
+    Plot a single arbitrary feature using the generalizable plotting function.
+    """
+    return generalPlotting([Plottable(data, dataColumn, " ", color, underlaidColor)],
+                           title, xAxisLabel, yAxisLabel, xlim, ylim,
+                           dropZeroRows, smoothData, overlaySmoothedAndNormal)
+
+
+def plotFeatureWithCustomNucleosomes(data: pandas.DataFrame, nucleosomePositions: List[int], dataColumn = "Normalized_Both_Strands",
+                                     title = "Misc Data", xAxisLabel = "Position Relative to Feature (bp)", yAxisLabel = "Counts",
+                                     xlim = None, ylim = None, dropZeroRows = True, smoothData = True, overlaySmoothedAndNormal = True,
+                                     nucleosomeColorPalette = NucleosomeColorPalette(), blacklistedRegions = None):
+    """
+    Plots a single arbitrary feature with coloring for given nucleosome positions.
+    Uses the generalizable plotting function.
+    """
+    data = data.copy()
+    data["Color"] = nucleosomeColorPalette.linker
+    data["Underlaid_Color"] = nucleosomeColorPalette.linkerUnderlaid
+    if blacklistedRegions is not None:
+        data.loc[data["Dyad_Position"].isin(blacklistedRegions),"Color"] = PlottingColors.BLACK.value
+        data.loc[data["Dyad_Position"].isin(blacklistedRegions),"Underlaid_Color"] = PlottingColors.BLACK_UNDERLAY.value
+    for nucleosomePosition in nucleosomePositions:
+        data.loc[data["Dyad_Position"].isin(range(nucleosomePosition-73,nucleosomePosition+73+1)),"Color"] = nucleosomeColorPalette.nucleosomal
+        data.loc[data["Dyad_Position"].isin(range(nucleosomePosition-73,nucleosomePosition+73+1)),"Underlaid_Color"] = nucleosomeColorPalette.nucleosomalUnderlaid
+
+    return generalPlotting([Plottable(data, dataColumn, " ", None, None, "Color", "Underlaid_Color",
+                                      {nucleosomeColorPalette.nucleosomal:"Nucleosomal", nucleosomeColorPalette.linker:"Linker"})],
+                           title, xAxisLabel, yAxisLabel, xlim, ylim,
+                           dropZeroRows, smoothData, overlaySmoothedAndNormal)
+
+
+def plotReadLengthDistribution(data: pandas.DataFrame, readLengthDataCol = "Sequence_Length", readCountsDataCol = "Read_Count",
+                               title = "Read Length Distribution", xAxisLabel = "Read Length", yAxisLabel = "Number of Reads",
+                               xlim = None, xAxisBreaks = True, ylim = None, yAxisBreaks = True) -> ggplot:
+    """
+    Plots read length distribution using an enrinched indices data frame.
+    """
+    readLengthPlot = (
+        ggplot(data, aes(x = readLengthDataCol, y = readCountsDataCol, fill = '"black"')) +
+        geom_bar(stat = "identity") + scale_fill_identity() +
+        coord_cartesian(xlim = xlim, ylim = ylim) +
+        scale_x_continuous(breaks = xAxisBreaks) + scale_y_continuous(breaks = yAxisBreaks, expand = flushXAxis) + 
+        xlab(xAxisLabel) + ylab(yAxisLabel) + ggtitle(title) +
+        theme(figure_size=(9,6)) + defaultTextScaling + blankBackground
+    )
+
+    return readLengthPlot
+
+
+def plotEnrichedEndices(data: pandas.DataFrame, readLengthDataCol = "Sequence_Length", maxFrequencyDataCol = "G_Max_Frequency",
+                        maxFrequenceyPosDataCol = "G_Max_Frequency_Position",
+                        title = "Enriched indices", xAxisLabel = "Read Length", yAxisLabel = "Maximum Guanine Frequency",
+                        xlim = None, xAxisBreaks = True, ylim = None, yAxisBreaks = True) -> ggplot:
+    """
+    Plots relative enrichment of sequences at each read length using an enriched indices data frame.
+    """
+
+    enrichedIndicesPlot = (
+        ggplot(data, aes(x = readLengthDataCol, y = maxFrequencyDataCol, fill = '"black"')) +
+        geom_bar(stat = "identity") + scale_fill_identity() +
+        geom_text(aes(label = maxFrequenceyPosDataCol), nudge_y = 0.02, format_string = "{:.0f}") +
+        coord_cartesian(xlim = xlim, ylim = ylim) +
+        scale_x_continuous(breaks = xAxisBreaks) + scale_y_continuous(breaks = yAxisBreaks, expand = flushXAxis) + 
+        xlab(xAxisLabel) + ylab(yAxisLabel) + ggtitle(title) +
+        theme(figure_size=(9,6)) + defaultTextScaling + blankBackground
+    )
+
+    return enrichedIndicesPlot
+
+
+def plotIndividualFrequencies(data: pandas.DataFrame, readLengthDataCol = "Sequence_Length", positionDataCol = "Position",
+                              nucFreqDataCol = "G_Frequency", validReadLengths = range(22,30),
+                              title = "Guanine Frequency By Read Length", xAxisLabel = "Position Relative to 3' End", yAxisLabel = "Guanine Frequency",
+                              xlim = None, ylim = None) -> ggplot:
+    """
+    Plots Individual nucleotide frequencies across a given set of read lengths.
+    """
+
+    data = data.loc[(readLength in validReadLengths for readLength in data[readLengthDataCol])]
+
+    individualFrequenciesPlot = (
+        ggplot(data, aes(x = positionDataCol, y = nucFreqDataCol)) +
+        geom_bar(aes(fill = '"black"'), stat = "identity") + scale_fill_identity() +
+        facet_grid(readLengthDataCol) +
+        coord_cartesian(xlim = xlim, ylim = ylim) + scale_y_continuous(expand = flushXAxis) +
+        xlab(xAxisLabel) + ylab(yAxisLabel) + ggtitle(title) +
+        theme(figure_size=(9,1+1.5*len(validReadLengths))) + defaultTextScaling + blankBackground
+    )
+
+    return individualFrequenciesPlot
